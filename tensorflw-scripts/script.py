5b65ce01c685a09177ab55b922ce0d6ad5d27eee
Gilberto Medeiros <62359000+gilbertomoj@users.noreply.github.com>
2021-11-22 12:39:18 -0300

    Update script.py

--- tensorflw-scripts/script.py
+++ tensorflw-scripts/script.py
@@ -1,8 +1,24 @@
-import tensorflow_hub as hub
-import cv2
-import numpy
-import pandas as pd
 import tensorflow as tf
+from tensorflow import keras
+import numpy as np
 import matplotlib.pyplot as plt
 
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
 
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(16):
+    plt.subplot(4,4,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
