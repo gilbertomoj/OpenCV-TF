42b6885e7da28643f497b3362e4fd8f15b3afb5e
gilbertomoj <gibamedeirosgc@gmail.com>
2021-12-09 05:20:38 -0300

    novo

--- testes/tensorflw-scripts/script.py
+++ testes/tensorflw-scripts/script.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD:testes/tensorflw-scripts/script.py
 import tensorflow_hub as hub
 import cv2
 import numpy
@@ -6,3 +7,30 @@ import tensorflow as tf
 import matplotlib.pyplot as plt
 
 
+=======
+#https://www.tensorflow.org/tutorials/keras/classification?hl=pt-br
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+fashion_mnist = keras.datasets.fashion_mnist
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+               'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+
+train_images = train_images / 255.0
+
+test_images = test_images / 255.0
+
+plt.figure(figsize=(10,10))
+for i in range(16):
+    plt.subplot(4,4,i+1)
+    plt.xticks([])
+    plt.yticks([])
+    plt.grid(False)
+    plt.imshow(train_images[i], cmap=plt.cm.binary)
+    plt.xlabel(class_names[train_labels[i]])
+plt.show()
+>>>>>>> c607e0cd47cdd24e56ae0d1cca79a485ca246ac4:tensorflw-scripts/script.py
